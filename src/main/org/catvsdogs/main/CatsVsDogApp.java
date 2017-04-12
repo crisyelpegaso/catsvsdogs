ab5574960152ce208b10ae6d590c26413fca9238
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2017-04-12 17:09:47 -0300

    Update

--- src/main/org/catvsdogs/main/CatsVsDogApp.java
+++ src/main/org/catvsdogs/main/CatsVsDogApp.java
@@ -1,14 +1,10 @@
 package org.catvsdogs.main;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 public class CatsVsDogApp {
 
